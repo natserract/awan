e658acea35feb1e26443f09168138fdbc95950ab
natserract <benjaminstwo@gmail.com>
2021-12-30 09:39:45 +0800

    short article for references

--- server/lib/cli.rs
+++ server/lib/cli.rs
@@ -1,3 +1,14 @@
+/// [Rust] about references
+///
+/// Immutable references &T, which allow sharing but not mutation. 
+/// There can be multiple &T references to the same value simultaneously, 
+/// but the value cannot be mutated while those references are active.
+///
+/// Mutable references &mut T, which allow mutation but not sharing. 
+/// If there is an &mut T reference to a value, there can be no other active references 
+/// at that time, but the value can be mutated.
+///
+
 use super::s3::list_s3_objects;
 use super::system_io;
 use super::types::Result as ResultT;
